dd7966f716bbed90a5d264208f483464a9b26432
wairimu cicillia <wairimu cicillia@WairimuCicillia>
2023-09-20 11:57:48 +0300

    Initial Commit

---
+++ first/src/patternstrucuture/Assignment1.java
@@ -0,0 +1,10 @@
+package patternstrucuture;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		System.out.println("C026-01-0925/2022");
+
+	}
+
+}
---
+++ first/src/patternstrucuture/star.java
@@ -0,0 +1,15 @@
+package patternstrucuture;
+
+public class star {
+
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++) {
+			
+		}
+        for(int j=0;j<5;j++) {
+        	System.out.print("*");
+        }
+        System.out.println("*");
+	}
+
+}
